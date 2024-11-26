822b0d17822392d77c7238bc1f991befcbc704e4
Volodymyr Kozar <vovakozar@gmail.com>
2024-11-26 11:00:30 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <iframe
+        src="https://app.dev.walkns.com/embed/2"
+        width="100%"
+        height="500px"
+      ></iframe>
+    </div>
+  );
+}
