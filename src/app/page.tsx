493deb42f75f7d2dc3fb1f71f0cc05234980b4c3
Volodymyr Kozar <vovakozar@gmail.com>
2024-11-26 14:06:51 +0200

    Delete same-origin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
         src="https://app.dev.walkns.com/embed/2"
         width="100%"
         height="500px"
-        allow="same-origin"
       ></iframe>
     </div>
   );
